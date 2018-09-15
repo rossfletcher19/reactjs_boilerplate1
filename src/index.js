3944ce5719e1cf1afab5ab589d6aea287acb6424
Ross Fletcher <rtbfletch@outlook.com>
2018-09-15 15:06:36 -0700

    update dependency with security vulnerability

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 // create a new component and this component should produce some html.
 const App = function() {
-    return <div>HI!!!!</div>;
+    return <div>NEW REACT APP</div>;
 }
 
 // take this component generated html and show it in the DOM
