6c4c24e97b5245144753cedea1e11183836b9f7f
Ross Fletcher <rtbfletch@outlook.com>
2018-09-15 11:20:18 -0700

    initial commit with starter code and app loading

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a new component and this component should produce some html.
+const App = function() {
+    return <div>HI!!!!</div>;
+}
+
+// take this component generated html and show it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
